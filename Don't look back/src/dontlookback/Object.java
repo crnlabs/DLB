ad2c143c79b15f571827792b588df9615d647751
VidMas25 <anthelizondo@hotmail.com>
2014-02-15 15:07:50 -0500

    Added some methods to Object.

--- Don't look back/src/dontlookback/Object.java
+++ Don't look back/src/dontlookback/Object.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package dontlookback;
 
-/**
- *
- * @author Carl
- */
 public interface Object {
-    
+    public int width();
+    public int height();
+    public int depth();
+    public int weight();
 }
