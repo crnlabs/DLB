b2959179c01e0b2595d492c3d6cf7cdac4c1a254
Gameaday <onelastrhythm@gmail.com>
2014-05-10 15:49:48 -0400

    commit to being the best
    SHARE EVERYTHING

--- Don't look back/src/dontlookback/DontLookBack.java
+++ Don't look back/src/dontlookback/DontLookBack.java
@@ -11,9 +11,8 @@ public class DontLookBack {
      */
     public static void main(String[] args) {
         
-        //DLB_Graphics awesomeGraphics=new DLB_Graphics();'
+        DLB_Graphics awesomeGraphics=new DLB_Graphics();
         
-        System.out.println("Don't look back.");
         
     }
         //That command will send "This is going to commit"
