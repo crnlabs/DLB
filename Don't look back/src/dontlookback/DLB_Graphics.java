8e62e97b936697e887242c3e7ef6dec91379169f
StrangeBard <williamsjames4@gmail.com>
2014-05-10 11:12:35 -0500

    Properly set up lwjgl

---
+++ Don't look back/src/dontlookback/DLB_Graphics.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dontlookback;
+
+//this makes graphics 2d for now will change later
+import java.awt.Graphics;
+
+/**
+ *
+ * @author willi_000
+ * I haven't done this in a LONG time like 5+ years so this is going to take
+ * some time. -StrangeBard (James)
+ */
+public class DLB_Graphics {
+    
+    private Graphics window;
+    
+}
