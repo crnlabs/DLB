6dc60d46956a14b3bf59e51c14934561adda6274
Gameaday <onelastrhythm@gmail.com>
2014-05-25 17:10:40 -0400

    no more bipeds class

--- Don't look back/src/dontlookback/Biped.java
+++ Don't look back/src/dontlookback/Biped.java
@@ -2,4 +2,39 @@ package dontlookback;
 
 public interface Biped {
 
+    public float speed();
+
+    public double weight();
+
+    public boolean jump();
+
+    public int jumpHeight();
+
+    public int height(); //default to 1.75
+
+    public int width();
+
+    public int depth();
+
+    public float positionX();
+
+    public float positionY();
+
+    public float positionZ();
+
+    public float rotX();
+
+    public float rotY();
+
+    public float rotZ();
+
+    public Object[] inverntory();
+
+    public Object heldItem();
+
+    public boolean isHolding();
+
+    public int reactionTime();
+
+    public boolean gender();
 }
--- Don't look back/src/dontlookback/Bipeds.java
+++
@@ -1,5 +0,0 @@
-package dontlookback;
-
-public interface Bipeds {
-
-}
